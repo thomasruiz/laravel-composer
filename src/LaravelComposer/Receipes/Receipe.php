11d7843b7610395de68dda76bcac60cb20558ab6
Thomas Ruiz <contact@thomasruiz.eu>
2015-02-02 23:23:15 +0000

    Remove unused imports

--- src/LaravelComposer/Receipes/Receipe.php
+++ src/LaravelComposer/Receipes/Receipe.php
@@ -1,8 +1,6 @@
 <?php namespace LaravelComposer\Receipes;
 
 use LaravelComposer\Composer;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 interface Receipe
 {
